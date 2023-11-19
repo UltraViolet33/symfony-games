4a4cdf07a33af2b059ac03525d76061a31c4ed0d
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-11-19 09:36:44 +0100

    write tests for registration controller

--- tests/Controller/RegistrationControllerTest.php
+++ tests/Controller/RegistrationControllerTest.php
@@ -10,51 +10,44 @@ class RegistrationControllerTest extends WebTestCase
     public function testRegistrationFormRendering()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/register');
+
+        $client->request('GET', '/register');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Register');
-        $this->assertCount(1, $crawler->filter('form[name="registration_form"]'));
     }
 
-    // public function testSuccessfulRegistration()
-    // {
-    //     $client = static::createClient();
 
-    //     $crawler = $client->request('GET', '/register');
+    public function testSuccessfulRegistration()
+    {
+        $client = static::createClient();
 
-    //     $form = $crawler->selectButton('Register')->form();
+        $crawler = $client->request('GET', '/register');
+        $form = $crawler->selectButton('Register')->form();
 
-    //     $form['registration_form[email]'] = 'test@example.com';
+        $form['registration_form[email]'] = 'test1@example.com';
+        $form['registration_form[plainPassword][first]'] = '12345678';
+        $form['registration_form[plainPassword][second]'] = '12345678';
+        $form['registration_form[agreeTerms]'] = True;
+        $client->submit($form);
 
-    //     $form['registration_form[plainPassword][first]'] = 'password';
-    //     $form['registration_form[plainPassword][second]'] = 'password';
-    //     $form['registration_form[agreeTerms]'] = True;
-
-
-    //     $client->submit($form);
-
-    //     $this->assertResponseRedirects('/');
-    // }
+        $this->assertResponseRedirects('/');
+    }
 
 
-    // public function testFailedRegistration()
-    // {
-    //     $client = static::createClient();
+    public function testFailedRegistration()
+    {
+        $client = static::createClient();
 
-    //     $crawler = $client->request('GET', '/register');
+        $crawler = $client->request('GET', '/register');
+        $form = $crawler->selectButton('Register')->form();
 
-    //     $form = $crawler->selectButton('Register')->form();
+        $form['registration_form[email]'] = 'test@example.com';
+        $form['registration_form[plainPassword][first]'] = 'password1';
+        $form['registration_form[plainPassword][second]'] = 'password';
+        $form['registration_form[agreeTerms]'] = True;
 
-    //     $form['registration_form[email]'] = 'invalid-email';
-    //     $form['registration_form[plainPassword][first]'] = 'password';
-    //     $form['registration_form[plainPassword][second]'] = 'password';
-    //     $form['registration_form[agreeTerms]'] = True;
-
-    //     $client->submit($form);
-
-    //     $this->assertResponseIsSuccessful();
-    //     // $this->assertSelectorTextContains('div.invalid-feedback', 'The password fields must match.');
-    //     $this->assertSelectorTextContains('div.invalid-feedback', 'You should agree to our terms.');
-    // }
+        $client->submit($form);
+        $this->assertSelectorTextContains('div.invalid-feedback', 'The password fields must match.');
+    }
 }
