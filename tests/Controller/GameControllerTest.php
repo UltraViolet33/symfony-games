ec0febc5f8e0b6f9daea0a2a2a4b2af5ebbce5a0
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-11-19 10:04:00 +0100

    start to test gameController

---
+++ tests/Controller/GameControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+class GameControllerTest extends WebTestCase
+{
+
+    public function testRegistrationFormRendering()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Cherche un jeu :');
+    }
+
+}
